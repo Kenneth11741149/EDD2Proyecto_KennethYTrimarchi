f0b450b0f67c543fa0972ea2750a02767aba39b4
Kenneth <burningion@hotmail.com>
2019-05-21 18:52:26 -0500

    Creating project folder

---
+++ EDD2_KennethYTrimarchi_Proyecto/src/edd2_kennethytrimarchi_proyecto/EDD2_KennethYTrimarchi_Proyecto.java
@@ -0,0 +1,10 @@
+package edd2_kennethytrimarchi_proyecto;
+
+public class EDD2_KennethYTrimarchi_Proyecto {
+
+    
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+    
+}
