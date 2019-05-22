fcfc87c4ac40da418bdcd25539bbad80d99ad6ef
Kenneth <burningion@hotmail.com>
2019-05-21 20:51:09 -0500

    Introducing basic GUI

--- EDD2_KennethYTrimarchi_Proyecto/src/edd2_kennethytrimarchi_proyecto/EDD2_KennethYTrimarchi_Proyecto.java
+++ EDD2_KennethYTrimarchi_Proyecto/src/edd2_kennethytrimarchi_proyecto/EDD2_KennethYTrimarchi_Proyecto.java
@@ -1,7 +1,7 @@
 package edd2_kennethytrimarchi_proyecto;
 
 public class EDD2_KennethYTrimarchi_Proyecto {
-
+    
     
     public static void main(String[] args) {
         System.out.println("Proyecto de Estructura de Datos 2");
